c8c98907a2b996bfb0d232e6fd0e324cc688ddcc
spike <mr.spike200@gmail.com>
2016-12-07 02:39:15 +0100

    add basic menu

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import VisibleTodoList from './components/todo/TodoContainer'
+import AppBar from 'material-ui/AppBar'
 import Menu from './components/menu/Menu'
 import './App.css';
 
@@ -8,6 +9,7 @@ const App = () => (
   <MuiThemeProvider>
     <div className="App">
       <Menu />
+      <AppBar title="React to-do list"/>
       <div className="appWrapper">
         <VisibleTodoList />
       </div>
