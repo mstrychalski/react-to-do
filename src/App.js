ab043757d3f0419523dfa1836f71e300791cef47
spike <mr.spike200@gmail.com>
2016-12-07 02:36:35 +0100

    light theme

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import VisibleTodoList from './components/todo/TodoContainer'
-import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import Menu from './components/menu/Menu'
 import './App.css';
 
 const App = () => (
-  <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+  <MuiThemeProvider>
     <div className="App">
       <Menu />
       <div className="appWrapper">
